930d3e6fa779a6369312a0198494ed5bba6997d2
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-05-17 13:56:19 -0300

    Update service worker cache version to v34

--- SW.js
+++ SW.js
@@ -1,5 +1,5 @@
 // Aumentamos a versão para v30 para forçar o celular a limpar o erro antigo
-const CACHE_NAME = 'glicogabi-v33'; 
+const CACHE_NAME = 'glicogabi-v34'; 
 
 // Deixamos APENAS os arquivos que realmente existem no seu projeto agora
 const ASSETS = [
